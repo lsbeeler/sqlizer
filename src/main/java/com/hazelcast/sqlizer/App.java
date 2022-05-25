b8631a6cabfcb003fbf9727429424a3ebb3882a2
Charles Harris <charris@HZLCST-MBP-21.local>
2022-05-25 12:18:37 +0300

    added generic record objects

--- src/main/java/com/hazelcast/sqlizer/App.java
+++ src/main/java/com/hazelcast/sqlizer/App.java
@@ -3,6 +3,8 @@ package com.hazelcast.sqlizer;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.nio.serialization.GenericRecord;
+import com.hazelcast.nio.serialization.GenericRecordBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,6 +36,17 @@ public class App implements Runnable
             System.err.println("unable to read configuration XML: " + e.getMessage());
             System.exit(127);
         }
+        
+        // Creating New Compact Objects
+        GenericRecord employeeOne = GenericRecordBuilder.compact("employee")
+        			.setString("name", "Charles Harris")
+        			.setInt32("id", 100)
+        			.build();
+        
+        GenericRecord employeeTwo = GenericRecordBuilder.compact("employee")
+    			.setString("name", "Lucas Beeler")
+    			.setInt32("id", 110)
+    			.build();
 
     }
 }
