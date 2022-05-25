e9015a65d0ef72c285416ae98a4582ad733ca8de
Lucas Beeler <lsbeeler@Hazelnut.local>
2022-05-25 11:25:50 +0300

    Added basic app skeleton.

--- src/main/java/com/hazelcast/sqlizer/App.java
+++ src/main/java/com/hazelcast/sqlizer/App.java
@@ -1,13 +1,36 @@
 package com.hazelcast.sqlizer;
 
+import com.hazelcast.client.config.XmlClientConfigBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App implements Runnable
 {
+    public App()
+    {
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.run();
+    }
+
+    @Override
+    public void run( )
+    {
+        try {
+            XmlClientConfigBuilder configBuilder =
+                    new XmlClientConfigBuilder(new File("src/main/resources/client.xml"));
+        } catch (IOException e) {
+            System.err.println("unable to read configuration XML: " + e.getMessage());
+            System.exit(127);
+        }
+
     }
 }
